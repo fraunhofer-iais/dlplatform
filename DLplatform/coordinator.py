1ea3a92de49926f2b9db80a78b7540d0855c5858
Michael Kamp <michael.kamp@iais.fraunhofer.de>
2020-10-06 14:48:12 +1100

    Fixed a bug with the new minStartNodes mechanism: If minStartNodes is large (we could reproduce the bug with 21), then sending the initial model through sendAggregatedModel for all nodes in _waitingNodes seems to congest rabbitMQ. All messages will be send (the coordinator logs say so), but only few (2-5) ever reach the workers. By inserting a small waiting time here (sleep(0.1), i.e., 100 milliseconds) the communication channel does not get congested (as tested with 21 workers).
    We might need a test with larger numbers of workers.

--- DLplatform/coordinator.py
+++ DLplatform/coordinator.py
@@ -5,7 +5,7 @@ from DLplatform.synchronizing import Synchronizer
 
 from pickle import loads
 from multiprocessing import Queue
-import sys
+import sys, time
 
 '''
     The InitializationHandler defines, how the coordinator handles model parameters when new learners register. 
@@ -236,8 +236,9 @@ class Coordinator(baseClass):
             # we send around the initial parameters only when all the expected nodes are there
             # in case when parameter is not set, it is equal to 0 - so every new node will satisfy the condition
             if len(self._waitingNodes) >= self._minStartNodes:
-                for id in self._waitingNodes:
-                    self._communicator.sendAveragedModel(identifiers = [id], param = self._waitingNodes[id], flags = {"setReference":True})
+                for worker_id in self._waitingNodes:
+                    time.sleep(0.1)#without the sleep rabbitMQ gets congested and messages do not get delivered to nodes (occurred with 21 nodes and BatchLearners)
+                    self._communicator.sendAggregatedModel(identifiers = [worker_id], param = self._waitingNodes[worker_id], flags = {"setReference":True})
                 self._waitingNodes.clear()
                 # we want to allow to wait for 10 nodes, but then others to join dynamically
                 self._minStartNodes = 0
@@ -323,7 +324,7 @@ class Coordinator(baseClass):
                 elif not params is None:
                     # we do not want to update the nodes that are already inactive
                     nodesToSendAvg = list(set(nodes) & set(self._activeNodes))
-                    self._communicator.sendAveragedModel(nodesToSendAvg, params, flags)
+                    self._communicator.sendAggregatedModel(nodesToSendAvg, params, flags)
                     self._learningLogger.logBalancing(flags, self._nodesInViolation, list(self._balancingSet.keys()))
                     self._learningLogger.logAveragedModel(nodes, params, flags)
                     self._balancingSet.clear()
